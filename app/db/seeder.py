b4388990f458345d79a197f824047aa8c167beeb
yimiro <s_icay@naver.com>
2026-03-11 16:41:56 +0900

    seeder question.bulk except raise 수정

--- app/db/seeder.py
+++ app/db/seeder.py
@@ -1,10 +1,14 @@
+from typing import Literal
+
 from tortoise import Tortoise
 from app.db.database import TORTOISE_CONFIG
 from app.models.quote import Quote
 from app.models.question import Question
 
 
-async def save_to_db(data_list: list, data_type: str = 'quote'):
+
+
+async def save_to_db(data_list: list, data_type: Literal['quote', 'question'] ):
     await Tortoise.init(config=TORTOISE_CONFIG)
 
     try:
@@ -21,11 +25,13 @@ async def save_to_db(data_list: list, data_type: str = 'quote'):
             to_create = [
                 Question(content=content) for content in data_list
             ]
+            await Question.bulk_create(to_create)
 
         print(f'[성공] {data_type} 데이터 {len(data_list)}개가 DB에 저장되었습니다.')
 
     except Exception as e:
-        print(f'[실패] {data_type} 저장 중 오류 발생: {e}')
+        # print(f'[실패] {data_type} 저장 중 오류 발생: {e}')
+        raise f'[실패] {data_type} 저장 중 오류 발생: {e}'
 
     finally:
         await Tortoise.close_connections()
