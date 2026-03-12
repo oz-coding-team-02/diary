04e8ed18682cf04d61a9963cb0d5578f229c339d
RyuHawon <hfixjsj49@gmail.com>
2026-03-12 16:37:21 +0900

    로그아웃 구현, 로그아웃시 블랙리스트 토큰으로 저장하여 만료되지 않았어도 개인페이지 접근불가.

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,9 +1,11 @@
 from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
 from app.repositories.user_repo import UserRepo
+from app.core.security import hash_token
 from app.core.security import verify_password, create_access_token, decode_access_token
 from app.schemas.user import UserBase, TokenResponse
 from app.models.user import User
+from app.models.blacklisted_token import BlacklistedToken
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
@@ -13,20 +15,20 @@ class UserService:
         self.repo = repo
 
     async def signup_user(self, data: UserBase):
-        if await UserRepo.check_exists(data.useremail):
+        if await self.repo.check_exists(data.useremail):
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail="이미 등록된 이메일입니다.",
             )
 
-        return await UserRepo.create_user(
+        return await self.repo.create_user(
             useremail=data.useremail,
             password=data.password,
         )
 
 
     async def login_user(self, data: UserBase) -> TokenResponse:
-        user = await UserRepo.get_by_useremail(data.useremail)
+        user = await self.repo.get_by_useremail(data.useremail)
         if not user or not verify_password(data.password, user.password_hash):
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
@@ -43,10 +45,21 @@ def get_user_service(repo: UserRepo = Depends(get_user_repo)) -> UserService:
     return UserService(repo=repo)
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
-    useremail = decode_access_token(token)
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    repo: UserRepo = Depends(get_user_repo) # 앞서 만든 DI 함수 활용
+) -> User:
+    token_hash = hash_token(token)
+    is_blacklisted = await BlacklistedToken.filter(token_hash=token_hash).exists()
 
-    user = await UserRepo.get_by_useremail(useremail)
+    if is_blacklisted:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='로그아웃 상태입니다. 다시 로그인해주세요.'
+        )
+
+    useremail = decode_access_token(token)
+    user = await repo.get_by_useremail(useremail)
 
     if not user:
         raise HTTPException(
