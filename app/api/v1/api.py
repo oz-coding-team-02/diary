6af5d6c6a33af0c6eb3ea53ae6818c2239d4332c
yimiro <s_icay@naver.com>
2026-03-12 15:22:42 +0900

    명언 랜덤 출력, 북마크, 북마크 리스트

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter
 from app.api.v1.auth import router as auth_router
-from app.api.v1.users import router as user_router
 from app.api.v1.quote import router as quote_router
-
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
-api_router.include_router(user_router, prefix="/users", tags=["users"])
-api_router.include_router(quote_router, prefix="/quotes", tags=["quotes"])
+api_router.include_router(quote_router)
--- app/db/seeder.py
+++ app/db/seeder.py
@@ -6,27 +6,33 @@ from app.models.quote import Quote
 from app.models.question import Question
 
 
-async def save_to_db(data_list: list, data_type: Literal["quote", "question"]):
+
+
+async def save_to_db(data_list: list, data_type: Literal['quote', 'question'] ):
     await Tortoise.init(config=TORTOISE_CONFIG)
 
     try:
-        if data_type == "quote":
+        if data_type == 'quote':
             to_create = [
                 Quote(
-                    author=item.get("author", "Anonymous"), content=item.get("content")
-                )
-                for item in data_list
+                    author=item.get('author','Anonymous'),
+                    content=item.get('content')
+                ) for item in data_list
             ]
             await Quote.bulk_create(to_create)
 
-        elif data_type == "question":
-            to_create = [Question(content=content) for content in data_list]
+
+
+        elif data_type == 'question':
+            to_create = [
+                Question(content=content) for content in data_list
+            ]
             await Question.bulk_create(to_create)
 
-        print(f"[성공] {data_type} 데이터 {len(data_list)}개가 DB에 저장되었습니다.")
+        print(f'[성공] {data_type} 데이터 {len(data_list)}개가 DB에 저장되었습니다.')
 
     except Exception as e:
-        raise f"[실패] {data_type} 저장 중 오류 발생: {e}"
+        raise f'[실패] {data_type} 저장 중 오류 발생: {e}'
 
     finally:
         await Tortoise.close_connections()
