f3e633b34d70c142bc7f17ec8459efd97b05b9f1
RyuHawon <hfixjsj49@gmail.com>
2026-03-12 17:44:06 +0900

    병합충돌...

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -6,6 +6,6 @@ from app.services.user_service import get_current_user
 router = APIRouter()
 
 
-@router.get('/me', response_model=UserRead)
+@router.get("/me", response_model=UserRead)
 async def get_me(current_user: User = Depends(get_current_user)):
     return current_user
