bda9263d9e7910500b7ab8f0cc3cb0eb696ce954
RyuHawon <hfixjsj49@gmail.com>
2026-03-12 13:21:23 +0900

    deps.py 오타수정

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,5 +1,4 @@
 from fastapi import Depends, HTTPException, status
-from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from app.core.config import settings
