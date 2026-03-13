a13083328e42260318c243ff04535b01f035e68b
yimiro <s_icay@naver.com>
2026-03-13 15:57:04 +0900

    질문 중복 db

--- app/services/question_service.py
+++ app/services/question_service.py
@@ -12,13 +12,25 @@ class QuestionService:
     async def get_random_question_for_user(
         self, user_id: int
     ) -> Optional[QuestionRead]:
+
         seen_question_ids = await self.repo.get_seen_question_ids_for_user(user_id)
+
         question = await self.repo.get_random_question_excluding_ids(
             excluded_ids=seen_question_ids
         )
+
         if question:
-            return question
-        return await self.repo.get_random_question()
+            await self.repo.save_user_question_history(
+                user_id=user_id, question_id=question.id
+            )
+            return QuestionRead(content=question.content)
+
+        question = await self.repo.get_random_question()
+
+        if question:
+            return QuestionRead(content=question.content)
+
+        return None
 
 
 def get_question_repo() -> QuestionRepository:
